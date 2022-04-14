8613cdf0e126e9926e5c61ed9357f7b2de00c07d
Duy Khanh <ndkthienhy@gmail.com>
2022-04-14 09:14:03 +0700

    add addons: aard_stock_move, attachment_preview, product_documentation_set, sale_coupon, sale_coupon_delivery, sale_order_line

--- aard_invoice_report_template/models/res_company.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import models, fields
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-    sale_note_bank_info = fields.Text(string="Sale Note Bank Information")
