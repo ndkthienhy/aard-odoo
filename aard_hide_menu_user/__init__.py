ad4bb98e4726961ccd5dced5dd7347db8205d193
Duy Khanh <ndkthienhy@gmail.com>
2024-01-08 13:18:55 +0700

    add menu hide menu user on backend

---
+++ aard_hide_menu_user/__init__.py
@@ -0,0 +1,2 @@
+from . import cotrollers
+from . import models
---
+++ aard_hide_menu_user/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': "Hide Menu Back-end",
+    'summary': "Hide support, My odoo account on dropdown menu",
+    'description': """
+        Hide support, My odoo account on dropdown menu
+    """,
+    'category': 'Tool',
+    'author': "Nguyen Duy Khanh",
+    'company': 'Aardwolf Industries LLC',
+    'website': "https://toolrange.com.vn",
+    'version': '12.0.1.0.0.1',
+    'depends': [
+        "web"
+    ],
+    'data': [
+        'views/assets.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+    'support': 'khanh@aardwolf.com.vn',
+}
